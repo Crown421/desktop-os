e724ada8ddc06051aec14c81c04382dcb6251c89
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2025-02-04 23:03:54 +0000

    Remove bitwarden flatpak and get cli instead

---
+++ files/scripts/install-bw.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+url="https://vault.bitwarden.com/download/?app=cli&platform=linux"
+
+temp_dir=$(mktemp -dt bw.XXXXXX)
+trap 'rm -rf "$temp_dir"' EXIT INT TERM
+cd "$temp_dir"
+
+if ! curl -fsSL -o bw.zip "$url"; then
+    echo "Could not download archive"
+    exit 1
+fi
+
+# bindir='/usr/bin'
+bindir='/home/steffen/Code/desktop-os'
+
+unzip bw.zip
+
+mv bw "$bindir/"
+
+$bindir/bw --version
+
