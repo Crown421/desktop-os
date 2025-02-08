982948bae1bbaecf0060fa9449679329bf1f9e2f
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2025-02-08 09:46:57 +0000

    Typo

--- files/scripts/install-bw.sh
+++ files/scripts/install-bw.sh
@@ -5,20 +5,20 @@ set -oue pipefail
 # url="https://vault.bitwarden.com/download/?app=cli&platform=linux"
 url="https://github.com/bitwarden/sdk-sm/releases/download/bws-v0.5.0/bws-x86_64-unknown-linux-gnu-0.5.0.zip"
 
-temp_dir=$(mktemp -dt bw.XXXXXX)
+temp_dir=$(mktemp -dt bws.XXXXXX)
 trap 'rm -rf "$temp_dir"' EXIT INT TERM
 cd "$temp_dir"
 
-if ! curl -fsSL -o bw.zip "$url"; then
+if ! curl -fsSL -o bws.zip "$url"; then
     echo "Could not download archive"
     exit 1
 fi
 
 bindir='/usr/bin'
 
-unzip bw.zip
+unzip bws.zip
 
-mv bw "$bindir/"
+mv bws "$bindir/"
 
-$bindir/bw --version
+$bindir/bws --version
 
